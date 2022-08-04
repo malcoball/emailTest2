8566639db893f4d273d2a13eab721c5cd3a4dcaa
malcoball <adam_shaw1994@hotmail.co.uk>
2022-08-04 21:41:03 +0100

    html

---
+++ index.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email duh</title>
+</head>
+<body>
+    
+    <main>
+        <p>Send email</p>
+        <form class="contact-form" action="contactform.php" method="post">
+            <input type="text" name="name" placeholder="full name"> 
+            <input type="text" name="mail" placeholder="Your email ye"> 
+            <input type="text" name="subject" placeholder="Subject"> 
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Message in a bottle"></textarea>
+            <button type="submit" name="submit">SEND MAIL</button>
+        </form>
+    </main>
+
+</body>
+</html>
